d58d01a3268ef01173c791f5b93b271d1ee04f24
ClaudethMatute <150222554+ClaudethMatute@users.noreply.github.com>
2024-01-26 22:03:59 -0600

    creacion para base de datos

--- app/src/main/java/com/example/pm1ejercicio1/MainActivity.java
+++ app/src/main/java/com/example/pm1ejercicio1/MainActivity.java
@@ -15,51 +15,53 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-   EditText nombres,apellidos,telefono;
-   Button btnproceso;
+    EditText nombres, apellidos, telefono;
+    Button btnproceso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nombres=(EditText) findViewById(R.id.nombres);
-        apellidos=(EditText)findViewById(R.id.apellidos);
-        telefono=(EditText)findViewById(R.id.telefono);
-        btnproceso=(Button) findViewById(R.id.btnprocesar);
+        nombres = (EditText) findViewById(R.id.nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        telefono = (EditText) findViewById(R.id.telefono);
+        btnproceso = (Button) findViewById(R.id.btnprocesar);
 
 
         btnproceso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Bundle enviaDatos=new Bundle();
-                enviaDatos.putString("dato1",nombres.getText().toString());  // aqui guardamos el nombre en el paquete usando la  etiqueta "dato1 y asi para las demas".
-                enviaDatos.putString("dato2",apellidos.getText().toString());
-                enviaDatos.putString("dato3",telefono.getText().toString());
+                Bundle enviaDatos = new Bundle();
+                enviaDatos.putString("dato1", nombres.getText().toString());  // aqui guardamos el nombre en el paquete usando la  etiqueta "dato1 y asi para las demas".
+                enviaDatos.putString("dato2", apellidos.getText().toString());
+                enviaDatos.putString("dato3", telefono.getText().toString());
                 // Crear un "Intent" para indicar que queremos ir siguiente pantalla (ActivityPage)
-                Intent  intent=new Intent(MainActivity.this, ActivityPage.class);
+                Intent intent = new Intent(MainActivity.this, ActivityPage.class);
                 intent.putExtras(enviaDatos);// nos sirve para poder enviar los datos  desde una actividad de origen a una actividad de destino
                 startActivity(intent); //iniciamos la nueva pantalla
-
-                AddPerson();
-
             }
-
         });
-
+        AddPerson();
     }
 
     private void AddPerson() {
-            SQLiteConexion conexion = new   SQLiteConexion(this,Transaciones.DBName,null,1);
-            SQLiteDatabase db = conexion.getWritableDatabase(); //modo de escritura
+        SQLiteConexion conexion = new SQLiteConexion(this, Transaciones.DBName, null, 1);
+        SQLiteDatabase db = conexion.getWritableDatabase(); //modo de escritura
 
-            ContentValues valores=new ContentValues();
-            valores.put(Transaciones.nombre,nombres.getText().toString());
+        ContentValues valores = new ContentValues();
+        valores.put(Transaciones.nombre, nombres.getText().toString());
+        valores.put(Transaciones.apellidos, apellidos.getText().toString());
 
 
+        // Insertar datos en la tabla
+        Long resultado = db.insert(Transaciones.Tablepersonas, Transaciones.id, valores);
 
+        // Error al insertar
+        Toast.makeText(getApplicationContext(), "Registro Ingresado Correctamente", Toast.LENGTH_SHORT).show();
+
+
+        db.close();
     }
-
-
 }
